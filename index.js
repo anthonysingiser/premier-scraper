af9dc086fa8381f21e60c9d29331d4b3a3d6ae2d
chrysanthemum <asingiser@gmail.com>
2023-08-04 12:29:56 -0400

    create request for goal data

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = ;
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Premier Scraper API')
+})
+
+//GET list of top scorers
+app.get('/goals', async(req, res) => {
+    try {
+        const response = await request(`${baseUrl}&url=https://www.premierleague.com/stats/top/players/goals`)
+
+        res.json(JSON.parse(response))
+    
+    } catch (error) {
+        res.json(error)
+    }
 })
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
